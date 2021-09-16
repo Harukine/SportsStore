8bc9c4195c930f5344b1b9811218eab7a0506ac4
Jacob Nguyen <jacobhtnguyen@gmail.com>
2021-09-16 18:14:35 -0400

    created function to get page count

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -39,6 +39,12 @@ export class StoreComponent {
     this.changePage(1);
   }
 
+  get pageCount(): number {
+    return Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage
+    )
+  }
+
   get pageNumbers(): number[] {
     return Array(Math.ceil(this.repository
       .getProducts(this.selectedCategory).length / this.productsPerPage))
