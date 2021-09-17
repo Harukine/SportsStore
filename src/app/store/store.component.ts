7355be380cf0dae598b99266fc08b8689cf5fe28
Jacob Nguyen <jacobhtnguyen@gmail.com>
2021-09-17 17:04:04 -0400

    added navigation to addProductToCart method

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {Product} from "../model/product.model";
 import {ProductRepository} from "../model/product.repository";
 import {Cart} from "../model/cart.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: "store",
@@ -16,7 +17,8 @@ export class StoreComponent {
 
   constructor(
     private repository: ProductRepository,
-    private cart: Cart) {
+    private cart: Cart,
+    private router: Router) {
   }
 
   get products(): Product[] {
@@ -56,5 +58,6 @@ export class StoreComponent {
 
   addProductToCart(product: Product) {
     this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
   }
 }
