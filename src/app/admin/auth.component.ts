c12d71b69636794058151c77be1c7b8247986156
Jacob Nguyen <jacobhtnguyen@gmail.com>
2021-09-19 18:23:56 -0400

    wired up component to use auth service and authenicate credentials

--- src/app/admin/auth.component.ts
+++ src/app/admin/auth.component.ts
@@ -1,21 +1,31 @@
 import {Component} from "@angular/core";
 import {Router} from "@angular/router";
 import {NgForm} from "@angular/forms";
+import {AuthService} from "../model/auth.service";
 
 @Component({
   templateUrl: "auth.component.html"
 })
 export class AuthComponent {
-  public username: string | undefined;
-  public password: string | undefined;
+  // @ts-ignore
+  public username: string;
+  // @ts-ignore
+  public password: string;
   public errorMessage: string | undefined;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private auth: AuthService) {
+  }
 
   authenticate(form: NgForm) {
     if (form.valid) {
-      // perform authentication
-      this.router.navigateByUrl("/admin/main")
+      this.auth.authenticate(this.username, this.password)
+        .subscribe(response => {
+          if (response) {
+            this.router.navigateByUrl("/admin/main");
+          }
+          this.errorMessage = "Authentication Failed";
+        })
     } else {
       this.errorMessage = "Form Data Invalid";
     }
